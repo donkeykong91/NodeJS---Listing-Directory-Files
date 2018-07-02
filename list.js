bd3b1b8747faa5128aef7c5c859ca2fce7969093
donkeykong91 <darylnator@gmail.com>
2018-07-01 17:03:49 -0700

    Reading files and outputing them asynchronously

--- list.js
+++ list.js
@@ -1,5 +1,11 @@
 var fs = require("fs");
 
-var files = fs.readdirSync('./lib');
+fs.readdir('./lib', function (err,, files) {
+  if (err) {
+    throw err;
+  }
 
-console.log(files);
+  console.log(files);
+});
+
+console.log("Reading Files...");
